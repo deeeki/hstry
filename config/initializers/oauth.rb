aa06272d289a33ecadbd07d5d581721c93ecedcc
itzki <itzki.h@gmail.com>
2013-03-05 22:42:37 +0900

    Change Facebook permissions

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
-    scope: 'email,user_photos,read_stream'
+    scope: 'user_photos'
   provider :foursquare, ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET']
   provider :instagram, ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET']
 end
